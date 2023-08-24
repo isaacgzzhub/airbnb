25a89a526a7370990e3b846b30e9c591daa9fdcd
Isaac Gzz <isaacgzz.ing@gmail.com>
2023-08-24 10:33:47 -0600

    User Menu Greeting

--- frontend/src/components/Navigation/ProfileButton.js
+++ frontend/src/components/Navigation/ProfileButton.js
@@ -49,8 +49,8 @@ function ProfileButton({ user }) {
         <ul className={ulClassName} ref={ulRef}>
           {user ? (
             <>
+              <li>Hello, {user.firstName} {user.lastName}</li>
               <li>{user.username}</li>
-              <li>{user.firstName} {user.lastName}</li>
               <li>{user.email}</li>
               <li>
                 <button onClick={logout}>Log Out</button>
