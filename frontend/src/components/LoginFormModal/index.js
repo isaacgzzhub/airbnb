bb967e566244f7496faa82dc31018faed614b824
Isaac Gzz <isaacgzz.ing@gmail.com>
2023-08-24 10:22:03 -0600

    Log in Disable Function

--- frontend/src/components/LoginFormModal/index.js
+++ frontend/src/components/LoginFormModal/index.js
@@ -49,7 +49,12 @@ function LoginFormModal() {
         {errors.credential && (
           <p>{errors.credential}</p>
         )}
-        <button type="submit">Log In</button>
+        <button
+          type="submit"
+          disabled={credential.length < 4 || password.length < 6}
+        >
+          Log In
+        </button>
       </form>
     </>
   );
