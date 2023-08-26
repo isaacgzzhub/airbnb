caa86a5b19f17b9e406f80c8a01298cc1ecec666
Isaac Gzz <isaacgzz.ing@gmail.com>
2023-08-26 11:02:18 -0600

    Landing Page Fix

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,12 +1,14 @@
 import React, { useState, useEffect } from "react";
 import { useDispatch } from "react-redux";
-import { Switch } from "react-router-dom";
+import { Switch, Route } from "react-router-dom"; // Import Route from react-router-dom
 import * as sessionActions from "./store/session";
 import Navigation from "./components/Navigation";
+import LandingPage from "./components/LandingPage"; // Ensure this path matches where your LandingPage component is saved
 
 function App() {
   const dispatch = useDispatch();
   const [isLoaded, setIsLoaded] = useState(false);
+
   useEffect(() => {
     dispatch(sessionActions.restoreUser()).then(() => setIsLoaded(true));
   }, [dispatch]);
@@ -14,7 +16,12 @@ function App() {
   return (
     <>
       <Navigation isLoaded={isLoaded} />
-      {isLoaded && <Switch></Switch>}
+      {isLoaded && (
+        <Switch>
+          <Route path="/" exact component={LandingPage} />
+          {/* Add more routes as needed */}
+        </Switch>
+      )}
     </>
   );
 }
