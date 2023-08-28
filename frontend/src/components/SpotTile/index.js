ed94f10710844fa579edd7bf408d4d4577c57ba3
Isaac Gzz <isaacgzz.ing@gmail.com>
2023-08-28 10:55:45 -0600

    Tile Star Rating

--- frontend/src/components/SpotTile/index.js
+++ frontend/src/components/SpotTile/index.js
@@ -2,6 +2,9 @@ import React from "react";
 import "./SpotTile.css"; // for styling
 
 function SpotTile({ spot }) {
+  const ratingDisplay =
+    spot.avgRating && spot.avgRating > 0 ? spot.avgRating.toFixed(1) : "New";
+
   return (
     <div className="spot-tile" title={spot.name}>
       <img src={spot.previewImage} alt={spot.name} className="spot-thumbnail" />
@@ -9,6 +12,8 @@ function SpotTile({ spot }) {
         <span className="spot-name">{spot.name}</span>
         <span className="spot-city-state">{`${spot.city}, ${spot.state}`}</span>
         <span className="spot-price">{`${spot.price} / night`}</span>
+        <div className="spot-rating">⭐ {ratingDisplay}</div>{" "}
+        {/* Added the star emoji */}
       </div>
     </div>
   );
