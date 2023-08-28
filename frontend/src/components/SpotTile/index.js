ee471e37c15ef391e2fe6b1aa1f7d143d040ceb2
Isaac Gzz <isaacgzz.ing@gmail.com>
2023-08-28 11:01:39 -0600

    Landing Page: Complete

--- frontend/src/components/SpotTile/index.js
+++ frontend/src/components/SpotTile/index.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { Link } from "react-router-dom";
 import "./SpotTile.css"; // for styling
 
 function SpotTile({ spot }) {
@@ -6,16 +7,25 @@ function SpotTile({ spot }) {
     spot.avgRating && spot.avgRating > 0 ? spot.avgRating.toFixed(1) : "New";
 
   return (
-    <div className="spot-tile" title={spot.name}>
-      <img src={spot.previewImage} alt={spot.name} className="spot-thumbnail" />
-      <div className="spot-info">
-        <span className="spot-name">{spot.name}</span>
-        <span className="spot-city-state">{`${spot.city}, ${spot.state}`}</span>
-        <span className="spot-price">{`${spot.price} / night`}</span>
-        <div className="spot-rating">⭐ {ratingDisplay}</div>{" "}
-        {/* Added the star emoji */}
+    <Link
+      to={`/spots/${spot.id}`}
+      style={{ textDecoration: "none", color: "inherit" }}
+    >
+      <div className="spot-tile" title={spot.name}>
+        <img
+          src={spot.previewImage}
+          alt={spot.name}
+          className="spot-thumbnail"
+        />
+        <div className="spot-info">
+          <span className="spot-name">{spot.name}</span>
+          <span className="spot-city-state">{`${spot.city}, ${spot.state}`}</span>
+          <span className="spot-price">{`${spot.price} / night`}</span>
+          <div className="spot-rating">⭐ {ratingDisplay}</div>{" "}
+          {/* Added the star emoji */}
+        </div>
       </div>
-    </div>
+    </Link>
   );
 }
 
